f5252bc9a68fafb5316494e6c9d6d2e65adab47b
davfsa <davfsa@gmail.com>
2022-10-22 23:57:26 +0200

    Improve form errors (#17)
    - Fix some styling issues too

--- frontend/src/components/Card.tsx
+++ frontend/src/components/Card.tsx
@@ -1,6 +1,6 @@
 export default function Card({children}) {
     return (
-        <div className="flex relative px-10 min-h-screen min-w-screen">
+        <div className="flex relative px-10 min-h-screen min-w-screen overflow-hidden">
             <div className="relative p-4 w-full max-w-lg rounded-lg border shadow-md sm:p-6 md:p-8 bg-dark-mode-grey border-gray-700 m-auto">
                 <div className="absolute top-0 -left-4 w-72 h-72 bg-indigo-700 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob -z-10"></div>
                 <div className="absolute top-0 -right-4 w-72 h-72 bg-yellow-200 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob animation-delay-2000 -z-10"></div>
--- frontend/src/components/forms/SignUpForm.tsx
+++ frontend/src/components/forms/SignUpForm.tsx
@@ -19,21 +19,26 @@ export default function SignUpForm({setShowSignUp}) {
         validate: values => {
             const errors = {};
 
-            if (values.password != null && values.passwordConf != null) {
-                if (values.password != values.passwordConf) {
-                    setPasswordsMatch(false);
-                    // @ts-ignore
-                    errors.passwordConf = "Passwords do not match";
-                } else {
-                    setPasswordsMatch(true);
-                }
+            if (values.passwordConf !== "" && values.password != values.passwordConf) {
+                setPasswordsMatch(false);
+                // @ts-ignore
+                errors.passwordConf = "Passwords do not match";
+            } else {
+                setPasswordsMatch(true);
             }
-            if (values.username != null && /^[\w\-.]{5,40}$/.test(values.username)) {
+
+            if (values.username === "" || /^[\w\-.]{5,40}$/.test(values.username)) {
                 setUsernameValid(true);
             } else {
                 setUsernameValid(false);
-                // @ts-ignore
-                errors.username = "Username is invalid. May only contain: a-z, A-Z, 0-9, _, -, .";
+
+                if (values.username.length < 5 || values.username.length > 40) {
+                    // @ts-ignore
+                    errors.username = "Username must be between 5 and 40 characters in length";
+                } else {
+                    // @ts-ignore
+                    errors.username = "Username may only contain: a-z, A-Z, 0-9, _, -, .";
+                }
             }
 
             return errors;
@@ -68,38 +73,37 @@ export default function SignUpForm({setShowSignUp}) {
                 <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-300">Email</label>
                 <input id="email" name="email" type="email" onChange={formik.handleChange}
                        value={formik.values.email} placeholder="name@notcord.io" required={true}
-                       className="border text-sm rounded-lg block w-full p-2.5 bg-gray-600 border-gray-500 placeholder-gray-400 text-white"/>
+                       className="border text-sm rounded-lg block w-full p-2.5 bg-gray-600 border-gray-500 placeholder-gray-400 text-white outline-none"/>
             </div>
             <div>
                 <label htmlFor="username" className="block mb-2 text-sm font-medium text-gray-300">Username</label>
                 <input id="username" name="username" type="username" minLength={5} maxLength={40}
                        onChange={formik.handleChange} value={formik.values.username} placeholder="notcorduser"
                        required={true}
-                       className="border text-sm rounded-lg block w-full p-2.5 bg-gray-600 border-gray-500 placeholder-gray-400 text-white"/>
+                       className="border text-sm rounded-lg block w-full p-2.5 bg-gray-600 border-gray-500 placeholder-gray-400 text-white outline-none"/>
                 {usernameTaken ? <p className="text-rose-400 text-xs">Username is not available</p> : null}
-                {usernameValid ? null :
-                    <p className="text-rose-400 text-xs">Username is invalid. May only contain: a-z, A-Z, 0-9,
-                        _, -, .</p>}
+                {usernameValid ? null : <p className="text-rose-400 text-xs">{formik.errors.username}</p>}
             </div>
             <div>
                 <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-300">Password</label>
                 <input id="password" name="password" type="password" minLength={12} onChange={formik.handleChange}
                        value={formik.values.password} placeholder="••••••••" required={true}
-                       className="border text-sm rounded-lg block w-full p-2.5 bg-gray-600 border-gray-500 placeholder-gray-400 text-white"/>
+                       className="border text-sm rounded-lg block w-full p-2.5 bg-gray-600 border-gray-500 placeholder-gray-400 text-white outline-none"/>
             </div>
             <div>
                 <label htmlFor="passwordConf" className="block mb-2 text-sm font-medium text-gray-300">Confirm
                     Password</label>
                 <input id="passwordConf" name="passwordConf" type="password" onChange={formik.handleChange}
                        value={formik.values.passwordConf} placeholder="••••••••" required={true}
-                       className="border text-sm rounded-lg block w-full p-2.5 bg-gray-600 border-gray-500 placeholder-gray-400 text-white"/>
+                       className="border text-sm rounded-lg block w-full p-2.5 bg-gray-600 border-gray-500 placeholder-gray-400 text-white outline-none"/>
                 {passwordsMatch ? null :
-                    <p id="pass-conf-alert" className="text-rose-400 text-xs">Passwords do not match</p>}
+                    <p id="pass-conf-alert" className="text-rose-400 text-xs">{formik.errors.passwordConf}</p>}
             </div>
             {loading ? <SubmitButtonLoading/> : <SubmitButtonInteractible content={"Sign Up"}/>}
             <div className="text-sm font-medium text-gray-300">
-                Already registered? <a onClick={() => setShowSignUp(false)}
-                                       className="hover:underline text-indigo-500 cursor-pointer">Sign In</a>
+                Already registered?
+                <a onClick={() => setShowSignUp(false)}
+                   className="hover:underline text-indigo-500 cursor-pointer">Sign In</a>
             </div>
         </form>
     );
