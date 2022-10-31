0e2d35567b26ce459d36584dc82984f59ebbf920
thomm.o <olijdthompson@gmail.com>
2022-10-31 12:02:33 +0000

    Move jwt secret into a configuration class

--- backend/src/main/java/io/github/tandemdude/notcord/utils/JwtUtil.java
+++ backend/src/main/java/io/github/tandemdude/notcord/utils/JwtUtil.java
@@ -1,9 +1,9 @@
 package io.github.tandemdude.notcord.utils;
 
+import io.github.tandemdude.notcord.config.JwtConfig;
 import io.jsonwebtoken.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.time.Instant;
@@ -16,10 +16,10 @@ import java.util.Optional;
 public class JwtUtil {
     private static final Logger logger = LoggerFactory.getLogger(JwtUtil.class);
 
-    private final String jwtSecret;
+    private final JwtConfig jwtConfig;
 
-    public JwtUtil(@Value("${jwt.secret}") String jwtSecret) {
-        this.jwtSecret = jwtSecret;
+    public JwtUtil(JwtConfig jwtConfig) {
+        this.jwtConfig = jwtConfig;
     }
 
     public String generateToken(Map<String, Object> claims, long expiresInSeconds) {
@@ -27,13 +27,13 @@ public class JwtUtil {
         return Jwts.builder()
             .setClaims(newClaims)
             .setExpiration(new Date(Instant.now().toEpochMilli() + (expiresInSeconds * 1000)))
-            .signWith(SignatureAlgorithm.HS256, jwtSecret)
+            .signWith(SignatureAlgorithm.HS256, jwtConfig.getSecret())
             .compact();
     }
 
     public Optional<Map<String, Object>> parseToken(String token) {
         try {
-            return Optional.of(Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(token).getBody());
+            return Optional.of(Jwts.parser().setSigningKey(jwtConfig.getSecret()).parseClaimsJws(token).getBody());
         } catch (UnsupportedJwtException | MalformedJwtException | SignatureException | ExpiredJwtException ex) {
             logger.debug("Error occurred while decoding JWT token", ex);
         }
@@ -42,7 +42,7 @@ public class JwtUtil {
 
     public Optional<Map<String, Object>> parseTokenAllowExpired(String token) {
         try {
-            return Optional.of(Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(token).getBody());
+            return Optional.of(Jwts.parser().setSigningKey(jwtConfig.getSecret()).parseClaimsJws(token).getBody());
         } catch (UnsupportedJwtException | MalformedJwtException | SignatureException ex) {
             logger.debug("Error occurred while decoding JWT token", ex);
         } catch (ExpiredJwtException ex) {
