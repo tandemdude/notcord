30545ea12b555a6f37d027cbe6d7ac54725c6b26
thomm.o <olijdthompson@gmail.com>
2022-11-05 23:54:30 +0000

    Add root readme and license. Add license headers where necessary

--- services/notcord-commons/src/main/java/io/github/tandemdude/notcord/commons/enums/Scope.java
+++ services/notcord-commons/src/main/java/io/github/tandemdude/notcord/commons/enums/Scope.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2022 tandemdude
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package io.github.tandemdude.notcord.commons.enums;
 
 import lombok.Getter;
@@ -17,8 +33,18 @@ public class Scope {
 
     @Getter
     private static final Map<Long, String> scopeDescriptionMap = Map.of(
-        USER, "???", BOT, "???", IDENTITY_READ, "Read your username and avatar", GUILDS_READ, "See which guilds you are in",
-        GUILDS_JOIN, "Join guilds on your behalf", GUILDS_MEMBER_READ, "Read your nickname, roles, and permissions in the guilds you are in"
+        USER,
+        "???",
+        BOT,
+        "???",
+        IDENTITY_READ,
+        "Read your username and avatar",
+        GUILDS_READ,
+        "See which guilds you are in",
+        GUILDS_JOIN,
+        "Join guilds on your behalf",
+        GUILDS_MEMBER_READ,
+        "Read your nickname, roles, and permissions in the guilds you are in"
     );
 
     @Getter
